ce01c618107c6f42aa8f7c0ed3e2082c475d280a
Jake Cinco <jacobi.net@gmail.com>
2019-07-01 23:19:56 +0100

    Add 6 movie titles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,30 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to Kodflix!</h1>
-        <img src={residentEvil} className="App-logo" alt="Image of Resident Evil 1 movie" />
+        <img src={residentEvil} className="App-logo" alt="Resident Evil 1 movie" />
+      <div className='container'>
+        <div className='item'>
+          Movie 1
+        </div>
+        <div className='item'>
+          Movie 2
+        </div>
+        <div className='item'>
+          Movie 3
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='item'>
+          Movie 4
+        </div>
+        <div className='item'>
+          Movie 5
+        </div>
+        <div className='item'>
+          Movie 6
+        </div>
+      </div>
 
     </div>
   );
