a2b4afbcaf99ffdac900c600e0bf75b0c1a37fca
Jake Cinco <jacobi.net@gmail.com>
2019-07-11 22:42:46 +0100

    Introduce routing and navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Gallery from './Gallery.js';
+import Details from './Details.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Welcome to Kodflix!</h1>
-        <Gallery />
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path='/' component={Gallery} />
+          <Route exact path='/details' component={Details} />
+        </div>
+      </Router>
     );
   }
 }
 
 export default App;
+
+
--- src/Show.js
+++ src/Show.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Show(props) {
     return (
-    <div className='item'>
-      <img src={props.coverphoto} className="App-logo" alt={`${props.title}`} />
-      <div className='overlay'>
-        <h3>{props.title}</h3>
-      </div>
-    </div>
+        <Link to='/details' className='item'>
+            <img src={props.coverphoto} className="App-logo" alt={`${props.title}`} />
+            <div className='overlay'>
+                <h3>{props.title}</h3>
+            </div>
+        </Link>
     );
-  }
+}
