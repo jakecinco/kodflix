1ccfb405c0380ab184e4a68ba7eb990f0f910e4f
Jake Cinco <jacobi.net@gmail.com>
2019-07-12 16:20:20 +0100

    Keep a unique details URL for each film or show

--- src/Gallery.js
+++ src/Gallery.js
@@ -10,14 +10,14 @@ export default function Gallery() {
     return (
       <div>
         <div className='container'>
-          <Show title='Dawn of the Dead' coverphoto={dawnOftheDead} />
-          <Show title='Shawn of the Dead' coverphoto={shawnOftheDead} />
-          <Show title='The Girl with all the Gifts' coverphoto={tgwatg} />
+          <Show id='dawn-of-the-dead' title='Dawn of the Dead' coverphoto={dawnOftheDead} />
+          <Show id='shawn-of-the-dead' title='Shawn of the Dead' coverphoto={shawnOftheDead} />
+          <Show id='the-girl-with-all-the-gifts' title='The Girl with all the Gifts' coverphoto={tgwatg} />
         </div>
         <div className='container'>
-          <Show title='28 Days Later' coverphoto={twentyEightDaysLater} />
-          <Show title='World War Z' coverphoto={worldWarZ} />
-          <Show title='Train to Busan' coverphoto={trainToBusan} />
+          <Show id='28-days-later' title='28 Days Later' coverphoto={twentyEightDaysLater} />
+          <Show id='world-war-z' title='World War Z' coverphoto={worldWarZ} />
+          <Show id='train-to-busan' title='Train to Busan' coverphoto={trainToBusan} />
         </div>
       </div>
     );
--- src/Show.js
+++ src/Show.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Show(props) {
     return (
-        <Link to='/details' className='item'>
+        <Link to={`/${props.id}`} className='item'>
             <img src={props.coverphoto} className="App-logo" alt={`${props.title}`} />
             <div className='overlay'>
                 <h3>{props.title}</h3>
