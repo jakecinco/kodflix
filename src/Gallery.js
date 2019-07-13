4f8d9dfb07d3c5a25ec84905a6ab63bb6eb5256d
Jake Cinco <jacobi.net@gmail.com>
2019-07-13 09:18:24 +0100

    Improve the gallery layout

--- src/Gallery.js
+++ src/Gallery.js
@@ -13,8 +13,6 @@ export default function Gallery() {
           <Show id='dawn-of-the-dead' title='Dawn of the Dead' coverphoto={dawnOftheDead} />
           <Show id='shawn-of-the-dead' title='Shawn of the Dead' coverphoto={shawnOftheDead} />
           <Show id='the-girl-with-all-the-gifts' title='The Girl with all the Gifts' coverphoto={tgwatg} />
-        </div>
-        <div className='container'>
           <Show id='28-days-later' title='28 Days Later' coverphoto={twentyEightDaysLater} />
           <Show id='world-war-z' title='World War Z' coverphoto={worldWarZ} />
           <Show id='train-to-busan' title='Train to Busan' coverphoto={trainToBusan} />
