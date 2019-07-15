42f7dea299f7473c5386ea348ca2579b16765733
Jake Cinco <jacobi.net@gmail.com>
2019-07-15 14:54:06 +0100

    Create NotFound page to redirect not found pages

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import getShows from './shows-get.js';
 
 export default class Details extends Component {
@@ -19,12 +19,17 @@ export default class Details extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <h1>{this.state.movieTVshow.title}</h1>
-        <Link to='/'>Back to home page</Link>
-      </div>
-    );
+    if (this.state.movieTVshow === undefined) {
+      return <Redirect to='/not-found' />
+    } else {
+      return (
+        <div>
+          <h1>{this.state.movieTVshow.title}</h1>
+          <Link to='/'>Back to home page</Link>
+        </div>
+      );
+    }
+    
   }
 }
 
