5dfb66bf4fdf53eb5458845633c6e7a74336d15d
Jake Cinco <jacobi.net@gmail.com>
2019-08-13 22:58:04 +0100

    #18 Introduce a back-end server

--- src/frontend/not-found/NotFound.js
+++ src/frontend/not-found/NotFound.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function NotFound() {
     return (
-      <div className='container'>
+      <div>
         <h1>Ooops, it looks like this page doesn't exist :(</h1>
         <Link to='/'>Back to home page</Link>
       </div>
