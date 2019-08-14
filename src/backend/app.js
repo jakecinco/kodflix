002c7d37087d2610c873228eff8f08c22c4b3607
Jake Cinco <jacobi.net@gmail.com>
2019-08-14 22:57:52 +0100

    #22 Connect the front end and backend servers; #23 Enable the backend server in prod

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const path = require('path');
+const port = process.env.PORT || 3001;
 const getShows = require('./shows');
 
 app.get('/rest/shows', (req, res) => res.send(getShows()));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.use(express.static(path.join(__dirname, '../..build'))); //Serve static files
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../..build', 'index.html'))); //React routing
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
--- src/frontend/gallery/Gallery.js
+++ src/frontend/gallery/Gallery.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ShowCover from './show-cover/ShowCover';
 import getShows from '../getShows';
 import './Gallery.css';
 
-export default function Gallery() {
-    return (
-        <div className='gallery'>
-            {
-                getShows().map(show => (
-                    <ShowCover
-                        key={show.id} //Each child in a list should have a unique "key" prop.
-                        id={show.id}
-                        title={show.title}
-                        coverphoto={show.coverphoto} />
-                ))
-            }
-        </div>
-    );
+export default class Gallery extends Component {
+
+    componentDidMount() {
+        fetch('/rest/shows')
+            .then(res => res.json())
+            .then(showJSON => console.log(showJSON));
+    }
+
+    render() {
+        return (
+            <div className='gallery' >
+                {
+                    getShows().map(show => (
+                        <ShowCover
+                            key={show.id} //Each child in a list should have a unique "key" prop.
+                            id={show.id}
+                            title={show.title}
+                            coverphoto={show.coverphoto} />
+                    ))
+                }
+            </div>
+        );
+    }
+
+
 }
