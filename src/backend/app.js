bbbd2bfd56d97c55ac6426b6eb2cf4d5aa2edac5
Jake Cinco <jacobi.net@gmail.com>
2019-09-21 20:25:06 +0100

    #28 Connect the kodflix app with database

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,16 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
-const getShows = require('./shows');
+const db = require('./db');
 
-app.get('/rest/shows', (req, res) => res.send(getShows()));
+db.connect().then(dbo => {
+    app.get('/rest/shows', (req, res) => {
+        dbo.collection('shows').find({}).toArray((err, results) => {
+            if (err) throw err;
+            res.send(results);
+        })
+    });
+    //Serve static files from the build folder
+    app.use(express.static(path.join(__dirname, '../../build')));
 
-//Serve static files from the build folder
-app.use(express.static(path.join(__dirname, '../../build')));
-
-//Route the index.html file (the entry point of our app) on the build folder by default:
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+    //Route the index.html file (the entry point of our app) on the build folder by default:
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+    });
+    app.listen(port, () => console.log(`Kodflix app listening on port ${port}!`));
 });
-
-app.listen(port, () => console.log(`Kodflix app listening on port ${port}!`));
